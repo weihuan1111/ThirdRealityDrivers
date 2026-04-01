58276b1a11c06d94744cb28ed6cfa55ceb32b2c1
weihuan <huan.wei@thirdreality.ai>
2026-04-01 17:06:44 +0800

    add dirty level for air pressure sensor
    Change-Id: Ia641b8613e7c0f7e04b545c6d348f9de3a1a230f

--- thirdreality-air-pressure-sensor/src/init.lua
+++ thirdreality-air-pressure-sensor/src/init.lua
@@ -19,21 +19,56 @@ local clusters = require "st.zigbee.zcl.clusters"
 
 KEEN_PRESSURE_ATTRIBUTE = 0x0000
 
+local AnalogInput = clusters.AnalogInput
+
 local function pressure_report_handler(driver, device, value, zb_rx)
   local kPa = math.floor(value.value / 10)
   device:emit_event(capabilities.atmosphericPressureMeasurement.atmosphericPressure({value = kPa, unit = "kPa"}))
+end
+
+local function round(num)
+  local mult = 10
+  return math.floor(num * mult + 0.5) / mult
+end
+
+local function dirty_level_handler(driver, device, value, zb_rx)
+  local level_value = value.value
+  level_value = round(level_value)
+  local level_val = math.tointeger(level_value)
+  device:emit_component_event(device.profile.components["dirty-level"], capabilities.level.level(level_val))
+end
+
+local added_handler = function(self, device)
+  device:send(AnalogInput.attributes.PresentValue:read(device))
+end
+
+local do_refresh = function(self, device)
+  device:refresh()
+  device:send(AnalogInput.attributes.PresentValue:read(device))
 end
 
 local zigbee_driver = {
   supported_capabilities = {
     capabilities.battery,
     capabilities.atmosphericPressureMeasurement,
+    capabilities.level,
     capabilities.refresh
+  },
+  lifecycle_handlers = {
+    added = added_handler
+  },
+  capability_handlers = {
+    [capabilities.refresh.ID] = {
+      [capabilities.refresh.commands.refresh.NAME] = do_refresh,
+    }
   },
   zigbee_handlers = {
     attr = {
       [clusters.PressureMeasurement.ID] = {
         [KEEN_PRESSURE_ATTRIBUTE] = pressure_report_handler
+      },
+      [AnalogInput.ID] = {
+        [AnalogInput.attributes.PresentValue.ID] = dirty_level_handler
       },
     }
   }
